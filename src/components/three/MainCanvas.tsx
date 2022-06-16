5b29e9c60608c54da4c13c47bfbbafa435063ba7
大川貴則 <okawa.t@arsaga.jp>
2022-06-17 01:44:51 +0900

    feat: ライティング追加

--- src/components/three/MainCanvas.tsx
+++ src/components/three/MainCanvas.tsx
@@ -10,7 +10,9 @@ const MainCanvas = () => {
         {BoxPositions.map((data, index) => (
           <BoxGeometry position={data} key={index} />
         ))}
-        <ambientLight />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} />
+        <pointLight position={[10, 10, 0]} />
       </Canvas>
     </>
   )
