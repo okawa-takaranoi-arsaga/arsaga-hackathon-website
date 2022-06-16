6edf00c66122a8ad94bdb6ced0909d1cccca44a3
大川貴則 <okawa.t@arsaga.jp>
2022-06-17 02:07:50 +0900

    feat: 仮のカメラ角度、位置調整

---
+++ src/components/three/MainCamera.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useThree } from '@react-three/fiber'
+
+const MainCamera = () => {
+  useThree(({ camera }) => {
+    camera.position.set(0, 2, 4)
+    camera.rotation.set(-0.2, 0, 0)
+  })
+
+  return <></>
+}
+
+export default MainCamera
--- src/components/three/MainCanvas.tsx
+++ src/components/three/MainCanvas.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import BoxGeometry from './BoxGeometry'
 import { BoxPositions } from '../../modules/BoxPositions'
+import MainCamera from './MainCamera'
 
 const MainCanvas = () => {
   return (
@@ -10,6 +11,7 @@ const MainCanvas = () => {
         {BoxPositions.map((data, index) => (
           <BoxGeometry position={data} key={index} />
         ))}
+        <MainCamera />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} />
         <pointLight position={[10, 10, 0]} />
