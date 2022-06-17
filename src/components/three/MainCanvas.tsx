c7563aaf3426a42ec8c76275999c178f55351b92
大川貴則 <okawa.t@arsaga.jp>
2022-06-18 00:57:25 +0900

    feat: idを追加

--- src/components/three/MainCanvas.tsx
+++ src/components/three/MainCanvas.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import BoxGeometry from './BoxGeometry'
-import { BoxPositions } from '../../modules/BoxPositions'
+import { BOX_POSITIONS } from '../../modules/BoxPositions'
 import MainCamera from './MainCamera'
 
 const MainCanvas = () => {
   return (
     <>
       <Canvas>
-        {BoxPositions.map((data, index) => (
-          <BoxGeometry position={data} key={index} />
+        {BOX_POSITIONS.map((box) => (
+          <BoxGeometry position={box.position} key={box.id} />
         ))}
         <MainCamera />
         <ambientLight intensity={0.5} />
--- src/modules/BoxPositions.ts
+++ src/modules/BoxPositions.ts
@@ -1,11 +1,37 @@
 import { Vector3Tuple } from 'three'
 
-export const BoxPositions: Vector3Tuple[] = [
-  [-6, 0, -6],
-  [-4, 0, -6],
-  [-2, 0, -6],
-  [0, 0, -6],
-  [2, 0, -6],
-  [4, 0, -6],
-  [6, 0, -6],
+type BoxPositionsType = {
+  id: number
+  position: Vector3Tuple
+}
+
+export const BOX_POSITIONS: BoxPositionsType[] = [
+  {
+    id: 1,
+    position: [-6, 0, -6],
+  },
+  {
+    id: 2,
+    position: [-4, 0, -6],
+  },
+  {
+    id: 3,
+    position: [-2, 0, -6],
+  },
+  {
+    id: 4,
+    position: [0, 0, -6],
+  },
+  {
+    id: 5,
+    position: [2, 0, -6],
+  },
+  {
+    id: 6,
+    position: [4, 0, -6],
+  },
+  {
+    id: 7,
+    position: [6, 0, -6],
+  },
 ]
