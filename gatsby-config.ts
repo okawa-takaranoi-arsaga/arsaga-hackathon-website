b3fabae5342acddc79b72e977d8c581234eaafd7
y-yonezawa <yonezawa_yuki@arsaga.jp>
2022-06-18 16:22:47 +0900

    feat: Google Analyticsの設定

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,25 @@
 import type { GatsbyConfig } from "gatsby";
 
+const appUrl = process.env.APP_URL ?? "http://localhost:8000"
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `arsaga-hackathon-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: appUrl
   },
   plugins: ["gatsby-plugin-sass"]
 };
+
+if (appUrl === 'https://arsaga-hackathon-website.netlify.app') {
+  config.plugins?.push({
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      trackingIds: [process.env.GOOGLE_ANALYTICS_ID],
+      pluginConfig: {
+        head: true,
+      },
+    },
+  })
+}
 
 export default config;
